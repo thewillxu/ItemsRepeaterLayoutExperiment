fda8f782f73fc26d85c998fe408a7847ecd5bfc0
Kaz Okuda <kaokuda@microsoft.com>
2023-02-24 18:30:37 -0800

    Added LayoutItems to track the layout.

---
+++ LayoutItems.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Windows.Foundation;
+using static ItemRepeaterShiftedLayoutExample.LayoutItems;
+
+namespace ItemRepeaterShiftedLayoutExample
+{
+    public partial class LayoutItems
+    {
+        public class RectWithIndex
+        {
+            public int Index;
+            public Rect Rect;
+        }
+
+        private List<Rect> Items = new List<Rect>();
+        private int StartIndex = 0;
+
+        public Rect this[int i] => Items[i - StartIndex];
+
+        public int Count { get => Items.Count; }
+
+        public void Clear()
+        {
+            Items.Clear();
+            StartIndex = 0;
+        }
+
+        public void SetItem(int index, Rect item)
+        {
+            // For safety, ensure that the index is either within the current range of values or it is increasing the range by 1.
+
+            if (Count == 0)
+            {
+                Items.Insert(0, item);
+                StartIndex = index;
+            }
+            else if (index == StartIndex - 1)
+            {
+                Items.Insert(0, item);
+                StartIndex = index;
+            }
+            else if (index >= StartIndex && index < StartIndex + Count)
+            {
+                Items[index - StartIndex] = item;
+            }
+            else if (index == StartIndex + Count)
+            {
+                Items.Add(item);
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("index");
+            }
+        }
+    }
+
+    public partial class LayoutItems : IEnumerable<RectWithIndex>
+    {
+        public IEnumerator<RectWithIndex> GetEnumerator()
+        {
+            for (int i = 0; i < Items.Count; ++i)
+            {
+                yield return new RectWithIndex { Rect = Items[i], Index = StartIndex + i };
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
--- VirtualLayoutTest.cs
+++ VirtualLayoutTest.cs
@@ -1,24 +1,31 @@
 ﻿using Microsoft.UI.Xaml.Controls;
 using System;
 using System.Diagnostics;
+using System.Reflection;
+using Windows.ApplicationModel.Store;
 using Windows.Foundation;
+using static ItemRepeaterShiftedLayoutExample.LayoutItems;
 
 namespace ItemRepeaterShiftedLayoutExample
 {
 
+    public class Anchor
+    {
+        public Anchor(int index, double top)
+        {
+            Index = index;
+            Top = top;
+        }
+
+        public readonly int Index;
+        public readonly double Top;
+    }
+
     public class VirtualLayoutTest : VirtualizingLayout
     {
-        private struct Anchor
-        {
-            public Anchor(int index, double top)
-            {
-                Index = index;
-                Top = top;
-            }
-
-            public readonly int Index;
-            public readonly double Top;
-        }
+        private Anchor LastAnchor;
+        private LayoutItems LayoutItems = new LayoutItems();
+        private double AverageHeight = 100;
 
         protected override Size MeasureOverride(VirtualizingLayoutContext context, Size availableSize)
         {
@@ -27,17 +34,54 @@ namespace ItemRepeaterShiftedLayoutExample
             Anchor anchor = GetAnchor(context);
             var (lastIndex, lastBottom) = GenerateLayout(context, availableSize, anchor);
 
+            MeasureAndArrangeLayout(context);
+
             var remainingItems = context.ItemCount - lastIndex;
             var estimatedBottom = remainingItems >= 0 ? lastBottom + remainingItems * AverageHeight : lastBottom;
 
             return new Size(availableSize.Width, estimatedBottom);
         }
 
+        private void MeasureAndArrangeLayout(VirtualizingLayoutContext context)
+        {
+            foreach (var item in LayoutItems)
+            {
+                var element = context.GetOrCreateElementAt(item.Index);
+                element.Measure(new Size(item.Rect.Width, item.Rect.Height));
+                element.Arrange(item.Rect);
+            }
+        }
+
         private Anchor GetAnchor(VirtualizingLayoutContext context)
         {
+            RectWithIndex item = GetVisibleItem(context);
+            if (item != null)
+            {
+                return new Anchor(item.Index, item.Rect.Top);
+            }
+
             int estimatedIndex = Math.Max(0, (int)Math.Round(context.RealizationRect.Top / AverageHeight));
             double estimatedTop = estimatedIndex * AverageHeight;
-            return new Anchor(estimatedIndex, estimatedTop);
+            LastAnchor = new Anchor(estimatedIndex, estimatedTop);
+            return LastAnchor;
+        }
+
+        private RectWithIndex GetVisibleItem(VirtualizingLayoutContext context)
+        {
+            foreach (var item in LayoutItems)
+            {
+                if (DoesIntersect(item.Rect, context.RealizationRect))
+                {
+                    return item;
+                }
+            }
+
+            return null;
+        }
+
+        private bool DoesIntersect(Rect rectA, Rect rectB)
+        {
+            return rectA.Bottom >= rectB.Top && rectA.Top <= rectB.Bottom;
         }
 
         private (int, double) GenerateLayout(VirtualizingLayoutContext context, Size availableSize, Anchor anchor)
@@ -45,14 +89,15 @@ namespace ItemRepeaterShiftedLayoutExample
             var currentTop = anchor.Top;
             var index = anchor.Index;
 
-            while(currentTop <= context.RealizationRect.Bottom && index < context.ItemCount)
+            LayoutItems.Clear();
+
+            while (currentTop <= context.RealizationRect.Bottom && index < context.ItemCount)
             {
-                var element = context.GetOrCreateElementAt(index);
                 Item item = context.GetItemAt(index) as Item;
                 var width = availableSize.Width;
                 var height = item.Height;
-                element.Measure(new Size(width, height));
-                element.Arrange(new Rect(0, currentTop, width, height));
+
+                LayoutItems.SetItem(index, new Rect(0, currentTop, width, height));
 
                 currentTop += height;
                 index++;
@@ -74,6 +119,5 @@ namespace ItemRepeaterShiftedLayoutExample
             return (index, currentTop);
         }
 
-        private double AverageHeight = 100;
     }
 }
